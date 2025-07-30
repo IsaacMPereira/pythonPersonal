7bd1ca5d10e0fabf963ddb4f99eba5967aa98306
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-07-29 23:09:03 -0300

    Begining: Automate the boring stuff

---
+++ automateTheBoringStuff/c7/isPhoneNumber.py
@@ -0,0 +1,37 @@
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    
+    for i in range(0, 2):
+        if not text[i].isdecimal():
+            return False
+        
+    if text[3] != '-':
+        return False
+    
+    for i in range(4, 6):
+        if not text[i].isdecimal():
+            return False
+        
+    if text[7] != '-':
+        return False
+    
+    for i in range(8, 11):
+        if not text[i].isdecimal():
+            return False
+        
+    return True
+
+def main():
+    message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office'
+
+    for i in range(len(message)):
+        chunk = message[i:i+12]
+        if isPhoneNumber(chunk):
+            print('Phone number found: ' + chunk)
+    
+    print('Done')
+
+
+if __name__ == "__main__":
+    main()
